6543d0403d9edd7da7c77cbb993f090b1f1918dc
12234556 <saguturusiri@gmail.com>
2026-04-11 12:42:06 -0400

    check

--- src/test/java/SixthApril/IntClass.java
+++ src/test/java/SixthApril/IntClass.java
@@ -1,4 +1,34 @@
 package SixthApril;
 
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntClass {
-}
+    @Test
+    public void IntClass() {
+        List<Integer> demo = new ArrayList<>();
+        demo.add(1);
+        demo.add(2);
+        demo.add(3);
+        demo.add(4);
+        demo.add(5);
+        System.out.println(demo.size());//total 5
+        System.out.println(demo.add(6));//adding one more num
+        System.out.println(demo.get(demo.size() - 1));
+
+        demo.set(2, 5); // Replace index 2 (3rd element) with 5
+        System.out.println("After replacing index 2 with 5:");
+        for (Integer obj : demo) {
+            System.out.println(2);
+
+            demo.remove(3); // Removes element at index 3 (4th element, originally 4)
+            System.out.println("After removing index 3:");
+
+
+            }
+
+        }
+    }
+
